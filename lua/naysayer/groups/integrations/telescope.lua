bb6bebe77db79bc09e8f4e29335204f73ee004e4
Jaime Paredes <jjpp109309@gmail.com>
2024-04-08 10:02:47 -0600

    Update telescope.lua

--- lua/naysayer/groups/integrations/telescope.lua
+++ lua/naysayer/groups/integrations/telescope.lua
@@ -1,49 +1,6 @@
 local M = {}
 
 function M.get()
-	if O.integrations.telescope.style == "nvchad" then
-		return {
-			TelescopeBorder = {
-				fg = O.transparent_background and C.blue or C.darkest_green,
-				bg = O.transparent_background and C.none or C.darkest_green,
-			},
-			TelescopeMatching = { fg = C.yellow },
-			TelescopeNormal = {
-				bg = O.transparent_background and C.none or C.darkest_green,
-			},
-			TelescopePromptBorder = {
-				fg = O.transparent_background and C.blue or C.darker_green,
-				bg = O.transparent_background and C.none or C.darker_green,
-			},
-			TelescopePromptNormal = {
-				fg = C.brown,
-				bg = O.transparent_background and C.none or C.darker_green,
-			},
-			TelescopePromptPrefix = {
-				fg = C.monokai_yellow,
-				bg = O.transparent_background and C.none or C.darker_green,
-			},
-			TelescopePreviewTitle = {
-				fg = O.transparent_background and C.green or C.darkest_green,
-				bg = O.transparent_background and C.none or C.green,
-			},
-			TelescopePromptTitle = {
-				fg = O.transparent_background and C.red or C.darkest_green,
-				bg = O.transparent_background and C.none or C.red,
-			},
-			TelescopeResultsTitle = {
-				fg = O.transparent_background and C.grey or C.darkest_green,
-				bg = O.transparent_background and C.none or C.grey,
-			},
-			TelescopeSelection = {
-				fg = O.transparent_background and C.monokai_yellow or C.brown,
-				bg = O.transparent_background and C.none or C.darker_green,
-				style = { "bold" },
-			},
-			TelescopeSelectionCaret = { fg = C.monokai_yellow },
-		}
-	end
-
 	return {
 		-- TelescopeNormal = { link = "NormalFloat" }, -- Respect telescope's default float bg
 		TelescopeBorder = { link = "FloatBorder" },
