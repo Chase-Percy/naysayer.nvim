58c7f9479394382769089e04a48da8b917d4fce8
J. Jaime Paredes <jjpp109309@gmail.com>
2024-04-16 00:46:32 -0600

    Fix test

--- tests/init.lua
+++ tests/init.lua
@@ -1,21 +1,25 @@
 local status, error = pcall(function()
 	local root = vim.fn.fnamemodify(".repro", ":p")
+	print("root", root)
 	for _, name in ipairs { "config", "data", "state", "cache" } do
 		vim.env[("XDG_%s_HOME"):format(name:upper())] = root .. "/" .. name
 	end
 
 	local lazypath = root .. "/plugins/lazy.nvim"
+	print("lazypath", lazypath)
 	if not vim.loop.fs_stat(lazypath) then
 		vim.fn.system { "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath }
 	end
 	vim.opt.runtimepath:prepend(lazypath)
 
+	print("getinfo(1)", debug.getinfo(1).source)
+	print("weirth path", debug.getinfo(1).source:sub(2, -30))
 	require("lazy").setup({
 		{ "jameswalls/naysayer.nvim", dev = true },
 	}, {
 		root = root .. "/plugins",
 		dev = {
-			path = debug.getinfo(1).source:sub(2, -21),
+			path = debug.getinfo(1).source:sub(2, -30),
 		},
 	})
 
