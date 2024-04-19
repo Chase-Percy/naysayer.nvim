bfb3b10a96cc9f37678e1c169dc403d2d9df7e97
J. Jaime Paredes <jjpp109309@gmail.com>
2024-04-18 19:37:09 -0600

    cursorlihne

--- lua/reactive/presets/naysayer-naysayer-cursorline.lua
+++ lua/reactive/presets/naysayer-naysayer-cursorline.lua
@@ -6,51 +6,51 @@ return {
 	static = {
 		winhl = {
 			inactive = {
-				CursorLine = { bg = darken(palette.surface0, 0.85) },
-				CursorLineNr = { bg = darken(palette.surface0, 0.85) },
+				CursorLine = { bg = darken(palette.light_green, 0.85) },
+				CursorLineNr = { bg = darken(palette.light_green, 0.85) },
 			},
 		},
 	},
 	modes = {
 		i = {
 			winhl = {
-				CursorLine = { bg = darken(palette.sky, 0.4) },
-				CursorLineNr = { bg = darken(palette.sky, 0.4) },
+				CursorLine = { bg = darken(palette.light_green, 0.4) },
+				CursorLineNr = { bg = darken(palette.light_green, 0.4) },
 			},
 		},
 		n = {
 			winhl = {
-				CursorLine = { bg = palette.surface0 },
-				CursorLineNr = { bg = palette.surface0 },
+				CursorLine = { bg = palette.light_green },
+				CursorLineNr = { bg = palette.light_green },
 			},
 		},
 		no = {
 			operators = {
 				[{ "gu", "gU", "g~", "~" }] = {
 					winhl = {
-						CursorLine = { bg = palette.surface2 },
-						CursorLineNr = { bg = palette.surface2 },
+						CursorLine = { bg = palette.light_green },
+						CursorLineNr = { bg = palette.light_green },
 					},
 				},
 				-- delete operator
 				d = {
 					winhl = {
-						CursorLine = { bg = darken(palette.red, 0.4) },
-						CursorLineNr = { bg = darken(palette.red, 0.4) },
+						CursorLine = { bg = darken(palette.light_green, 0.4) },
+						CursorLineNr = { bg = darken(palette.light_green, 0.4) },
 					},
 				},
 				-- yank operator
 				y = {
 					winhl = {
-						CursorLine = { bg = darken(palette.peach, 0.4) },
-						CursorLineNr = { bg = darken(palette.peach, 0.4) },
+						CursorLine = { bg = darken(palette.light_green, 0.4) },
+						CursorLineNr = { bg = darken(palette.light_green, 0.4) },
 					},
 				},
 				-- change operator
 				c = {
 					winhl = {
-						CursorLine = { bg = darken(palette.blue, 0.5) },
-						CursorLineNr = { bg = darken(palette.blue, 0.5) },
+						CursorLine = { bg = darken(palette.light_green, 0.5) },
+						CursorLineNr = { bg = darken(palette.light_green, 0.5) },
 					},
 				},
 			},
@@ -58,20 +58,20 @@ return {
 		-- replace mode
 		R = {
 			winhl = {
-				CursorLine = { bg = darken(palette.sapphire, 0.5) },
-				CursorLineNr = { bg = darken(palette.sapphire, 0.5) },
+				CursorLine = { bg = darken(palette.light_green, 0.5) },
+				CursorLineNr = { bg = darken(palette.light_green, 0.5) },
 			},
 		},
 		-- visual mode
 		[{ "v", "V", "\x16" }] = {
 			winhl = {
-				Visual = { bg = darken(palette.mauve, 0.4) },
+				Visual = { bg = darken(palette.light_green, 0.4) },
 			},
 		},
 		-- select mode
 		[{ "s", "S", "\x13" }] = {
 			winhl = {
-				Visual = { bg = darken(palette.pink, 0.4) },
+				Visual = { bg = darken(palette.light_green, 0.4) },
 			},
 		},
 	},
