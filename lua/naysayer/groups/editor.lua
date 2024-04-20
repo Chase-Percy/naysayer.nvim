dc93e07a5d8e2d59dffe2663af2e2272708e6283
J. Jaime Paredes <jjpp109309@gmail.com>
2024-04-20 14:56:11 -0600

    Adjust blend

--- lua/naysayer/groups/editor.lua
+++ lua/naysayer/groups/editor.lua
@@ -25,8 +25,8 @@ function M.get()
 		SignColumnSB = { bg = C.darkest_green, fg = C.dark_green }, -- column where |signs| are displayed
 		Substitute = { bg = C.dark_green, fg = U.vary_color({ latte = C.red }, C.monokai_magenta) }, -- |:substitute| replacement text highlighting
 		-- LineNr = { fg = C.grey }, -- Line number for ":number" and ":#" commands, and when 'number' or 'relativenumber' option is set.
-		LineNr = {fg = U.blend(C.grey, O.transparent_background and "#000000" or C.darkest_green,  0.1) }, -- Line number for ":number" and ":#" commands, and when 'number' or 'relativenumber' option is set.
-		CursorLineNr = { fg = C.white, style = { "bold" } }, -- Like LineNr when 'cursorline' or 'relativenumber' is set for the cursor line. highlights the number in numberline.
+		LineNr = {fg = U.blend(C.grey, O.transparent_background and "#000000" or C.darkest_green,  0.65) }, -- Line number for ":number" and ":#" commands, and when 'number' or 'relativenumber' option is set.
+		CursorLineNr = { fg = C.grey, style = { "bold" } }, -- Like LineNr when 'cursorline' or 'relativenumber' is set for the cursor line. highlights the number in numberline.
 		MatchParen = { fg = C.monokai_orange, bg = C.dark_green, style = { "bold" } }, -- The character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
 		ModeMsg = { fg = C.white, style = { "bold" } }, -- 'showmode' message (e.g., "-- INSERT -- ")
 		-- MsgArea = { fg = C.white }, -- Area for messages and cmdline, don't set this highlight because of https://github.com/neovim/neovim/issues/17832
