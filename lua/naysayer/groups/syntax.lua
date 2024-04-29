9fbcf3126187d7cac95b739108afb9b364200025
Jaime Paredes <jjpp109309@gmail.com>
2024-04-29 14:39:06 -0600

    Update syntax.lua

--- lua/naysayer/groups/syntax.lua
+++ lua/naysayer/groups/syntax.lua
@@ -4,7 +4,7 @@ function M.get()
 	return {
 		Comment = { fg = C.green, style = O.styles.comments }, -- just comments
 		SpecialComment = { link = "Special" }, -- special things inside a comment
-		Constant = { fg = C.mint }, -- (preferred) any constant
+		Constant = { fg = C.brown }, -- (preferred) any constant
 		String = { fg = C.tiffany, style = O.styles.strings or {} }, -- a string constant: "this is a string"
 		Character = { fg = C.tiffany }, --  a character constant: 'c', '\n'
 		Number = { fg = C.mint, style = O.styles.numbers or {} }, --   a number constant: 234, 0xff
